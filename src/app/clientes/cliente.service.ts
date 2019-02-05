e2500f66dda06871ae2f7c39d5b099e6b685d582
Alejandro Exequiel Hernández Lara <alejandro@pckz.cl>
2019-02-05 12:52:47 -0300

    Metodo crear listo

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -3,13 +3,15 @@ import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
 
 @Injectable()
 export class ClienteService {
   private urlEndPoint: string = 'http://localhost:8080/api/clientes';
+  private httpHeaders = new HttpHeaders({'Content-Type' : 'application/json'});
+
   constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]> {
@@ -17,6 +19,10 @@ export class ClienteService {
     return this.http.get(this.urlEndPoint).pipe(
       map(response => response as Cliente[])
     );
+  }
+
+  create(cliente: Cliente) : Observable<Cliente> {
+    return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers: this.httpHeaders})
   }
 
 }
